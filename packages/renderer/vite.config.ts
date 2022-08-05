eec09195708663c5d9ae53419cb029ca48696a8a
Mike Orozco <me@mikeorozco.dev>
2022-08-04 20:47:00 -0700

    Use vite-plugin-electron-renderer

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import electronRenderer from 'vite-plugin-electron/renderer';
+import electronRenderer from 'vite-plugin-electron-renderer';
 import pkg from '../../package.json'
 
 export default defineConfig({
