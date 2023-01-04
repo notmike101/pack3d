55736ade40fedff53caf3c513d322e3604c0cee5
Mike Orozco <me@mikeorozco.dev>
2023-01-04 15:26:52 -0600

    Add border around log output

--- packages/renderer/src/components/Log.vue
+++ packages/renderer/src/components/Log.vue
@@ -8,7 +8,7 @@ const logString = computed<string>(() => logs.value.join('\n'));
 </script>
 
 <template>
-  <footer class="basis-[150px] text-left bg-[#f0f0f0] border-t border-t-black overflow-auto bloc max-h-[150px]">
+  <footer class="basis-[150px] text-left bg-[#f0f0f0] border border-black overflow-auto bloc max-h-[150px]">
     <textarea class="block p-[5px] m-0 outline-0 border-0 resize-none w-full h-full">{{ logString }}</textarea>
   </footer>
 </template>
